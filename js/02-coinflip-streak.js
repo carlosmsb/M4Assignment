befe8a3a8a0b8dc6e298e3f8af24eceb83927e0e
Admin <>
2023-10-10 22:15:39 -0700

    2ndCommit

---
+++ js/02-coinflip-streak.js
@@ -0,0 +1,11 @@
+let coinFlip = 0;
+
+do {
+    coinFlip = Math.round(Math.random());
+
+    if (coinFlip === 0) {
+        console.log("Heads");
+    } else if (coinFlip === 1) {
+        console.log("Tails");
+    }
+} while (coinFlip !== 1);
